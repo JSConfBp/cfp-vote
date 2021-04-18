e8e167474228001a739d951731836843e08957b1
szabolcsit <szabolcsit@hu.ibm.com>
2021-04-18 19:29:53 +0200

    debug: stat failures

--- server/services/users/read.js
+++ server/services/users/read.js
@@ -4,18 +4,27 @@ const ADMINS = JSON.parse(process.env.ADMINS)
 
 const getUser = (user) => fetch(`https://api.github.com/users/${user}?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`)
   .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    return data
+  })
 
 module.exports = async function () {
   const storedUsers = await store.smembers('users')
   const allUsers = storedUsers.concat(ADMINS)
-  const data = await Promise.all(allUsers.map(user => getUser(user)))
+  try {
+    const data = await Promise.all(allUsers.map(user => getUser(user)))
 
-  const users = data.map(user => {
-    if (ADMINS.includes(user.login)) {
-      user.admin = true
-    }
-    return user
-  })
+    const users = data.map(user => {
+      if (ADMINS.includes(user.login)) {
+        user.admin = true
+      }
+      return user
+    })
 
-  return users
+    return users
+  } catch(e) {
+    console.error(e)
+    throw e
+  }
 }
